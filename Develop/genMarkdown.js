278169efc4a341e0a0b2e9eb6301620f7bf8416d
ZachariahKB <zachariahakb@gmail.com>
2024-04-17 23:26:37 -0700

    MeRaid

--- Develop/genMarkdown.js
+++ Develop/genMarkdown.js
@@ -1,39 +1,40 @@
 function genMarkdown(data) {
+   // Construct the Markdown content using template literals
    return `
-# ${data.title}
-
-${data.licence}
-
-### Description
-
-${data.description}
-
-### Table of Contents
-* Installation
-
-## Installation
-To install, execute these commands:
-\`\`\`
-${data.installation}
-\`\`\`
-
-## Usage
-${data.usage}
-
-## License
-${data.license}
-
-## Contributing
-${data.contributing}
-
-## Test
-${data.test}
-
-## Questions
-For any questions, you can reach out to:
-Email: ${data.email}
-GitHub: ${data.github}
-`;
-}
-
-module.exports = genMarkdown;
+ # ${data.title}
+ 
+ ${data.license} // Insert license information
+ 
+ ### Description
+ 
+ ${data.description} // Insert project description
+ 
+ ### Table of Contents
+ * Installation
+ 
+ ## Installation
+ To install, execute these commands:
+ \`\`\`
+ ${data.installation} // Insert installation instructions
+ \`\`\`
+ 
+ ## Usage
+ ${data.usage} // Insert usage information
+ 
+ ## License
+ ${data.license} // Reiterate license information
+ 
+ ## Contributing
+ ${data.contributing} // Insert contribution guidelines
+ 
+ ## Test
+ ${data.test} // Insert test instructions
+ 
+ ## Questions
+ For any questions, you can reach out to:
+ Email: ${data.email} // Insert email address for contacting
+ GitHub: ${data.github} // Insert GitHub username for contacting
+ `;
+ }
+ 
+ module.exports = genMarkdown; // Export the genMarkdown function for use in other modules
